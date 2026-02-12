dea0ec588d1d6233c391f09ad147c0d06bd53d3d
Ali Haqiqi <aliz@foresightanalytics.ca>
2026-02-13 01:21:03 +0330

    Revert "feat: Add Intercom messenger widget to landing page"
    This reverts commit 582e1daad02daa19693b4035778b6219f668e58f.

--- services/landing-web/app/layout.tsx
+++ services/landing-web/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Montserrat, Merriweather, Ubuntu_Mono } from "next/font/google";
-import { IntercomProvider } from "@/components/IntercomProvider";
 import "./globals.css";
 
 const montserrat = Montserrat({
@@ -87,7 +86,6 @@ export default function RootLayout({
       </head>
       <body className={`${montserrat.variable} ${merriweather.variable} ${ubuntuMono.variable} antialiased font-sans`}>
         {children}
-        <IntercomProvider />
       </body>
     </html>
   );
